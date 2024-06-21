11b853db1243f3e0d4f9e8f49c804c09923b59c6
vladimirovicp <vladimirovicp70@gmail.com>
2024-06-21 14:43:38 +0400

    feat: add загрузка данных

--- contacts/src/main.jsx
+++ contacts/src/main.jsx
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import "./index.css";
 
-import Root from "./routes/root";
+import Root, { loader as rootLoader } from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact from "./routes/contact";
 
--- contacts/src/routes/root.jsx
+++ contacts/src/routes/root.jsx
@@ -1,4 +1,10 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLoaderData, } from "react-router-dom";
+import { getContacts } from "../contacts";
+
+export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
+  }
 
 export default function Root() {
     return (
@@ -29,16 +35,29 @@ export default function Root() {
             </form>
           </div>
           <nav>
+          {contacts.length ? (
             <ul>
-              <li>
-                <Link to={`contacts/1`}>Your Name</Link>
-              </li>
-              <li>
-                {/* <a href={`/contacts/2`}>Your Friend</a> */}
-                <Link to={`contacts/2`}>Your Friend</Link>
-              </li>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
             </ul>
-          </nav>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
+        </nav>
         </div>
         <div id="detail">
             <Outlet />
