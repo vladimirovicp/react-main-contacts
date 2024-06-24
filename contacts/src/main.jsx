0479b668e0cfc6ca91b05a28943e38ba459f69f5
vladimirovicp <vladimirovicp70@gmail.com>
2024-06-24 10:12:38 +0400

    feat: add

--- contacts/src/main.jsx
+++ contacts/src/main.jsx
@@ -9,7 +9,9 @@ import "./index.css";
 
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 import ErrorPage from "./error-page";
-import Contact from "./routes/contact";
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact";
 
 const router = createBrowserRouter([
   {
@@ -22,6 +24,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
       },
     ],
   },
