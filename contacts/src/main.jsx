be8ac0964496f56c49c54873bc155c8769268471
vladimirovicp <vladimirovicp70@gmail.com>
2024-06-25 16:07:15 +0400

    09. Параметры URL в загрузках

--- contacts/src/main.jsx
+++ contacts/src/main.jsx
@@ -10,7 +10,9 @@ import Root, {
   loader as rootLoader, 
   action as rootAction, 
 } from "./routes/root";
-import Contact from "./routes/contact";
+import Contact, {
+  loader as contactLoader,
+} from "./routes/contact";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +25,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
       },
     ]
   },
